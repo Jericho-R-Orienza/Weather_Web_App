dc3db134fd70a7d05423e71cb441b4a778a98217
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-02-15 10:12:39 -0500

    updated framework along with base functionality for simple window to search city weather

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+from waitress import serve
 
 #make our app a flask app
 app = Flask(__name__)
@@ -7,7 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    weather_data = get_current_weather(city)
+    #go into json file and return the desired vars we will be using
+    return render_template(
+        "weather.html",
+        title = weather_data["name"],
+        status = weather_data["weather"][0]["description"].capitalize(),
+        temp = f"{weather_data['main']['temp']:.1f}",
+        feels_like = f"{weather_data['main']['feels_like']:.1f}"
+    )
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8000)
+    serve(app, host = "0.0.0.0", port = 8000)
