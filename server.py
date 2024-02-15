8611a0a87e102c81e910daed8168d69776f20c6e
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-02-15 08:50:35 -0500

    framework1.0 - creating foundation of web application, testing server

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+
+#make our app a flask app
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8000)
